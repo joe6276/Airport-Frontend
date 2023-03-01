2c677025d8c1fbb448610cae9017aa97e6bfa756
Jonathan Ndambuki <joendambuki16@gmail.com>
2023-03-01 14:17:01 +0300

    Started on ngRX

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,26 @@
-import { NgModule } from '@angular/core';
+import { NgModule, isDevMode } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import {HttpClientModule, HTTP_INTERCEPTORS } from'@angular/common/http'
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TokenInterceptorService } from './Services/token-interceptor.service';
+import { StoreModule } from '@ngrx/store';
+import { sampleReducer } from './State/Reducers/sample';
+import { CounterReducer } from './State/Reducers/counterReducer';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+
 
 
 @NgModule({
   declarations: [
     AppComponent,
-
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    HttpClientModule
+    HttpClientModule,
+    StoreModule.forRoot({sample:sampleReducer, counter:CounterReducer}),
+    StoreDevtoolsModule.instrument({ maxAge: 25, logOnly: !isDevMode() })
   ],
   providers: [{provide:HTTP_INTERCEPTORS, useClass:TokenInterceptorService, multi:true}],
   bootstrap: [AppComponent]
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,5 +1,9 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { Store } from '@ngrx/store';
+import { AppState } from '../State/appState';
+import { CountState } from '../State/Reducers/counterReducer';
+import { DecreaseValue, IncreaseValue } from '../State/Actions/counterActions';
 
 @Component({
   selector: 'app-home',
@@ -8,6 +12,20 @@ import { CommonModule } from '@angular/common';
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent {
+export class HomeComponent  implements OnInit {
 
+  ngOnInit(): void {
+    this.store.select(CountState).subscribe(state=>{
+      // console.log(state);
+       this.count=state
+    })
+  }
+count!:number
+ constructor(private store:Store<AppState>){}
+  Add(){
+  this.store.dispatch(IncreaseValue({increaseBy:2}))
+  }
+  Minus(){
+    this.store.dispatch(DecreaseValue({decreaseBy:3}))
+  }
 }
